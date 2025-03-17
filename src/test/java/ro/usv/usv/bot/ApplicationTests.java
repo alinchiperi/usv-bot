33fba7da54b3b4df191ad7cf83910736d8e7a9bd
Alin Chiperi <chiperialin@yahoo.com>
2025-03-17 21:52:20 +0200

    Initial commit

---
+++ src/test/java/ro/usv/usv/bot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.usv.usv.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
